bbf91943bb0602f90e175f5c5e324daebec75aa1
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-17 16:55:00 -0700

    feat: Add create_layout method to board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,20 @@
 class Board
   attr_reader :layout
-  
+
   def initialize
     @layout = {}
+    create_layout
+  end
+
+  def create_layout
+    ("A".."G").to_a.each do |letter|
+      (1..6).each do |number|
+        @layout["#{letter}#{number}".to_sym] = "."
+      end
+    end
   end
 end
+
+# board = Board.new
+# board.create_layout
+# puts board.layout
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,9 +8,16 @@ RSpec.describe Board do
       expect(board).to be_a(Board)
     end
     
-    it 'contains a hash' do
+    it 'creates a hash' do
       expect(board.layout).to be_a(Hash) 
     end
+   
+    it 'builds a hash with 42 board positions as keys' do
+      keys_test = board.layout.count
+      
+      expect(keys_test).to eq(42)
+    end
+
   end
 
 end
