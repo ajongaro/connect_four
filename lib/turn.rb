7b99b0e9def815cc097f109853787c434fac2ea2
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-19 22:55:39 -0700

    refactor: drop_token method

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,21 +12,9 @@ class Turn
   end
 
   def drop_token(selected_column, player_token)
-    a_column_keys = [:A6, :A5, :A4, :A3, :A2, :A1]
-    b_column_keys = [:B6, :B5, :B4, :B3, :B2, :B1]
-    c_column_keys = [:C6, :C5, :C4, :C3, :C2, :C1]
-    d_column_keys = [:D6, :D5, :D4, :D3, :D2, :D1]
-    e_column_keys = [:E6, :E5, :E4, :E3, :E2, :E1]
-    f_column_keys = [:F6, :F5, :F4, :F3, :F2, :F1]
-    g_column_keys = [:G6, :G5, :G4, :G3, :G2, :G1]
-
-    if selected_column == "A"  then key_input = a_column_keys
-    elsif selected_column == "B"  then key_input = b_column_keys
-    elsif selected_column == "C"  then key_input = c_column_keys
-    elsif selected_column == "D"  then key_input = d_column_keys
-    elsif selected_column == "E"  then key_input = e_column_keys
-    elsif selected_column == "F"  then key_input = f_column_keys
-    elsif selected_column == "G"  then key_input = g_column_keys
+    
+    key_input = 6.downto(1).map do |number|
+      "#{selected_column}#{number}".to_sym
     end
 
     @board.layout.reverse_each do |key, value|
