fe106fc09a03a4d23d845aa84993467b13701f20
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-20 18:15:33 -0700

    feat: Add welcome message to start_game method and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,48 @@
 class Game 
   attr_reader :board
   def initialize
+    @board = nil
+    start_game
+  end
+
+  def start_game
+    # Beginning of program
+    # welcome message 
+    puts welcome_message 
+    # Add print laterChoice player quit or start
+    # create the board, and generate layout
     @board = Board.new
-    @board.create_layout
+  end
+
+
+  def welcome_message
+    "Welcome to CONNECT FOUR\nEnter 'p' to play. Enter 'q' to quit."
   end
 end 
+
+
+
+# LOOP START -----------------------------
+# Display game board
+
+# Prompt player for first piece
+
+# check for valid column
+# drop piece into proper position
+# if column is invalid, display error and prompt
+#for valid column
+
+# Display updated game board
+
+# computer iterates through to find valid col
+# computer places piece on board (O)
+
+# LOOP END ----------------------------
+# loops until game over conditions are met
+
+# 1) player wins
+# 2) computer wins
+# 3) no winner
+
+# print out results of game (you win, hopefully)
+# spit back to main menu (at top)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,12 +10,24 @@ RSpec.describe Game do
     end
     
     it 'creates a board' do
+      game.start_game
+
       expect(game.board).to be_a(Board)    
     end
   
     it 'creates a board layout' do
+      game.start_game
+
       expect(game.board.layout).to be_a(Hash)
       expect(game.board.layout[:A1]).to be_a(Space)
+    end
+  end
+
+  describe '#start_game' do
+    describe '#welcome_message' do
+      it 'prints out welcome message' do
+        expect(game.welcome_message).to eq("Welcome to CONNECT FOUR\nEnter 'p' to play. Enter 'q' to quit.")
+      end
     end
   end
 end
