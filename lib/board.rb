20bf0eea1b193448fdacfe506dbb2fecbc506c10
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-23 12:18:58 -0700

    fix: Refactor board class methods and clean up win check

--- lib/board.rb
+++ lib/board.rb
@@ -3,38 +3,36 @@ class Board
   attr_reader :layout, :header
 
   def initialize
-    @layout = {} # move this into create_layout method
     @header = "ABCDEFG" 
-    create_layout
+    @numbers = [1, 2, 3, 4, 5, 6] 
+    @r_numbers = [6, 5, 4, 3, 2, 1]
+    @letters = ["A", "B", "C", "D", "E", "F", "G"]
     @diag_start_keys = [:A4, :A5, :A6, :B6, :C6, :D6]
     @diag_start_keys2 = [:G4, :G5, :G6, :F6, :E6, :D6]
-    @diag_array = []
+
+    create_layout
   end
 
   def random_column
     @header.split("").sample
   end
 
-  # needs test - checks to see if col valid and top spot in col is avail
   def test_column(selection)
       @header.include?(selection) &&
       @layout["#{selection}#{'1'}".to_sym].include?(".")
   end
 
-  # build the board into @layout
   def create_layout
-    @layout = {} # move this into create_layout method
-    ("A".."G").to_a.each do |letter|
-      (1..6).each do |number|
+    @layout = {} 
+    @letters.each do |letter|
+      @numbers.each do |number|
         @layout["#{letter}#{number}".to_sym] = "."
       end
     end
   end
 
   def winner?
-    return true if horizontal_win
-    return true if vertical_win
-    return true if diagonal_win
+    return true if horizontal_win || vertical_win || diagonal_win
     false
   end
 
@@ -57,67 +55,70 @@ class Board
     array.join("")
   end
 
+  def check_for_four_in(line)
+    string = line.join("")
+    string.include?("XXXX") || string.include?("OOOO")
+  end
+
   def horizontal_win 
-    array = []
-    6.downto(1).to_a.each do |number|
-      ("A".."G").each do |letter|
-         array << @layout["#{letter}#{number}".to_sym] 
+    line = []
+    @r_numbers.each do |number|
+      @letters.each do |letter|
+         line << @layout["#{letter}#{number}".to_sym] 
       end
-      return true if array.join("").include?("XXXX") || array.join("").include?("OOOO") 
-      array.clear
+      return true if check_for_four_in(line) 
+      line.clear
     end
     false
   end
 
   def vertical_win 
-    array = []
-    ("A".."G").each do |letter|
-      6.downto(1).to_a.each do |number|
-         array << @layout["#{letter}#{number}".to_sym]
+    line = []
+    @letters.each do |letter|
+      @r_numbers.each do |number|
+         line << @layout["#{letter}#{number}".to_sym]
       end
-      return true if array.join("").include?("XXXX") || array.join("").include?("OOOO") 
-      array.clear
+      return true if check_for_four_in(line) 
+      line.clear
     end
     false
   end
 
   def diagonal_win
+    @diag_array = []
     @diag_start_keys.each do |key|
-      generate_diagonal_array(key, "up")
+      generate_diag_array(key, "up")
     end
  
     @diag_start_keys2.each do |key|
-      generate_diagonal_array(key, "down")
+      generate_diag_array(key, "down")
     end
 
-    diag_array = @diag_array.join("")
-
-    diag_array.include?("XXXX") || diag_array.include?("OOOO")
+    check_for_four_in(@diag_array)
   end
 
-  def generate_diagonal_array(start, direction)
-    current_location = start
-    until @layout[current_location].nil?
-      @diag_array << @layout[current_location]
-      current_location = start.to_s.split("")
-      current_location[0] = combine(current_location[0], direction)
-      current_location[1] = (current_location[1].to_i - 1).to_s
-      next_location = current_location.join("").to_sym
-        if @layout[next_location].nil?
+  # helper method for generate_diagonal_array
+  def increment(letter, direction)
+    if direction == "up"
+      (letter.ord + 1).chr
+    elsif direction == "down"
+      (letter.ord - 1).chr 
+    end
+  end
+
+  def generate_diag_array(start, direction)
+    node = start
+    until @layout[node].nil?
+      @diag_array << @layout[node]
+      node = start.to_s.split("")
+      node[0] = increment(node[0], direction)
+      node[1] = (node[1].to_i - 1).to_s
+      next_node = node.join("").to_sym
+        if @layout[next_node].nil?
           @diag_array << "NOPE"
           break
         end
-      generate_diagonal_array(next_location, direction)
-    end
-    
-  end
-
-  def combine(loc, direction)
-    if direction == "up"
-      return (loc.ord + 1).chr
-    elsif direction == "down"
-      return (loc.ord - 1).chr 
+      generate_diag_array(next_node, direction)
     end
   end
-
 end
--- lib/game.rb
+++ lib/game.rb
@@ -5,56 +5,18 @@ class Game
 
   def initialize
     @board = Board.new # remove this from here and generate new board in new_board
-    # main_menu_prompt
   end
 
   def new_board 
     @board = Board.new
   end
 
-  def game_play 
-    print_board
-    while !@board.winner?
-      puts "\n"
-      player_prompt
-      if @board.winner?
-        print_board
-        puts "\nYou Win!\n\n"
-        new_board
-        return main_menu_prompt
-      elsif @board.tie?
-        print_board
-        puts "\nTie Game!\n\n"
-        new_board
-        return main_menu_prompt
-      end
-
-      computer_turn
-      if @board.winner?
-        print_board
-        puts "\nYou Lose!\n\n"
-        new_board
-        return main_menu_prompt 
-      elsif @board.tie?
-        print_board
-        puts "\nTie Game!\n\n"
-        new_board
-        return main_menu_prompt
-      end
-      print_board
-    end
+  def print_board
+    print @board.pretty_print
   end
-
-  # def tie_game
-  #   print_board
-  #   puts "\nTie Game!\n\n"
-  #   new_board
-  #   return main_menu_prompt
-  # end
 
   def welcome_message
     "Welcome to CONNECT FOUR\nEnter 'p' to play. Enter 'q' to quit."
-    # consider just looping "Enter 'p' etc every failed start"
   end
 
   def main_menu_prompt
@@ -75,12 +37,12 @@ class Game
     begin
       exit
     rescue SystemExit
-      puts "Everybody quits, eventually..."
+      puts "Everyone quits, eventually..."
     end
   end
 
   def player_prompt
-    print "Please Select a Column: "
+    print "Please select a column: "
 
     column_selection = gets.chomp.upcase
     puts "\n"
@@ -89,7 +51,7 @@ class Game
     if turn.valid_column?
       turn.drop_token(column_selection, "X") 
     else
-      puts "Please Make a Valid Selection"
+      puts "Please make a valid selection"
       player_prompt
     end
   end
@@ -104,33 +66,39 @@ class Game
       computer_turn
     end
   end
+  
+  def game_play 
+    print_board
 
-  def print_board
-    print @board.pretty_print
+    while !@board.winner?
+      puts "\n"
+      player_prompt
+      if @board.winner?
+        print_board
+        puts "\nYou Win!\n\n"
+        new_board
+        return main_menu_prompt
+      elsif @board.tie?
+        print_board
+        puts "\nTie Game!\n\n"
+        new_board
+        return main_menu_prompt
+      end
+
+      computer_turn
+
+      if @board.winner?
+        print_board
+        puts "\nYou Lose!\n\n"
+        new_board
+        return main_menu_prompt 
+      elsif @board.tie?
+        print_board
+        puts "\nTie Game!\n\n"
+        new_board
+        return main_menu_prompt
+      end
+      print_board
+    end
   end
 end
-
-# LOOP START -----------------------------
-# Display game board
-
-# Prompt player for first piece
-
-# check for valid column
-# drop piece into proper position
-# if column is invalid, display error and prompt
-#for valid column
-
-# Display updated game board
-
-# computer iterates through to find valid col
-# computer places piece on board (O)
-
-# LOOP END ----------------------------
-# loops until game over conditions are met
-
-# 1) player wins
-# 2) computer wins
-# 3) no winner
-
-# print out results of game (you win, hopefully)
-# spit back to main menu (at top)
