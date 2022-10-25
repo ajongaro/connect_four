49cc0449173d84cc991015fa48dfb21020fc9e30
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-25 14:57:51 -0700

    test: Add test for add_record method

--- lib/record.rb
+++ lib/record.rb
@@ -1,8 +1,21 @@
 
 class Record
-  attr_reader :file
+  attr_reader :file, :records
 
   def initialize
     @file = File.open("./assets/record.txt")
+    @records = []
+  end
+
+  def print_records
+    File.foreach(@file) do |line|
+      record_line = line.split(",")
+      @records << record_line
+    end
+    @records
   end
 end
+
+
+
+# player_name,games_started,wins,losses
--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Record do
       expect(record.file).to be_a(File)
     end
   end
+  
+  describe '#display_results' do
+    it 'prints file line by line' do
+      record = Record.new
+      record.print_records
+
+      expect(record.records[0]).to eq(["player_name", "games_started", "wins", "losses"])
+    end
+  end
+
+  describe '#add_record' do
+    it 'adds a line to the record' do
+      record = Record.new
+      record.add_record("Anthony", 0, 1)
+
+      expect(record.records[1]).to eq(["Anthony", 1, 0, 1])
+    end
+  end
 end
